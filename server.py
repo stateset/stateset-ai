401693b273533bd26c570065eff85e99890b96cd
domsteil <domsteil14@gmail.com>
2023-01-25 18:14:32 -0800

    updating

---
+++ server.py
@@ -0,0 +1,20 @@
+import numpy as np
+from flask import Flask, request, jsonify
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('gpt-model.pkl','rb'))
+
+
+@app.route('/api',methods=['POST'])
+def completions():
+    data = request.get_json(force=True)
+    
+    prediction = model.predict([[np.array(data['exp'])]])
+    
+    output = prediction[0]
+
+    return jsonify(output)
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
